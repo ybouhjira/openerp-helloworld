ad3b876b6fd8d11d1f51ffaf3f9a86ae88e74124
ybouhjira <b.youssef91@gmail.com>
2014-06-19 12:41:26 +0100

    __openerp__.py file

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+  'name' : 'Hello world!',
+  'version' : '1.0',
+  'category' : 'Demos',
+  'description' : 'Just a useless OpenERP module'
+  'installable': True,
+  'application': True,
+  'auto_install': False,
+}

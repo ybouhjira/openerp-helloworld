81a91b22efb40472d3813ecf2fc586fca3b38331
ybouhjira <b.youssef91@gmail.com>
2014-06-20 12:52:47 +0100

    Added author

--- __init__.py
+++ __init__.py
@@ -1,11 +1,56 @@
 from osv import osv, fields
 
-class Message(osv.osv):
-  """ A message like 'Hello world' or something """
+class Author(osv.Model):
+  """ An author """
   
-  _name = 'helloworld.message'
+  _name = 'helloworld.author'
 
   _columns = {
-    'content' : fields.char('Content', size=64, required=True, translate=True)
+    'firstName' : fields.char(
+      string = 'First name',
+      size = 30,
+      required = True
+    ),
+    'lastName' : fields.char(
+      string = 'Last name',
+      size = 30,
+      required = True
+    ),
+    'book_ids' : fields.one2many(
+      obj = 'helloworld.book',
+      fields_id = 'author_id',
+      string = 'Books'
+    )
   }
-Message()
+
+class Book(osv.Model) :
+  """ A book """
+
+  _name = 'helloworld.book'
+  
+  _columns = {
+    'isbn' : fields.char(
+      string = 'ISBN',
+      size = 9,
+      required = True
+    ),
+    'title' : fields.char(
+      string = 'Title', 
+      size = 100,
+      required = True
+    ),
+    'author_id' : fields.many2one(
+      obj = 'helloworld.author',
+      string = 'Author',
+      required = True
+    )
+  }
+
+  # _sql_constraints = [
+  #   ('unique_isbn', 'unique(isbn)', 'The ISBN must be unique'),
+  #   ('unique_title', 'unique(title)', 'The title must be unique')
+  # ] 
+
+
+Author()
+Book()
